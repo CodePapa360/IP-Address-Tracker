461e30021e418a6f45fdd4dadb34d4284f39cef1
Alamin <codepapa360@gmail.com>
2023-08-03 02:25:11 +0600

    basic-html

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,4 @@
 "use strict";
 import "../sass/main.scss";
+//   const query = "69.53.84.173";
+//   const url = `http://ip-api.com/json/${query}?fields=status,message,country,regionName,city,zip,lat,lon,offset,isp,query`;
