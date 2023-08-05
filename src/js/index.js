398eab2f00d487f7c58ff0788ecb1ff7b03e4b77
Alamin <codepapa360@gmail.com>
2023-08-06 01:54:55 +0600

    improved-attribution

--- src/js/index.js
+++ src/js/index.js
@@ -9,20 +9,20 @@ import { requestApi } from "./utils/requestApi.js";
   const form = document.getElementById("form");
   const inputEl = document.querySelector(".dearch-input");
 
-  setTimeout(() => {
-    const myProfile = document.querySelector(".my-profile");
-    myProfile.classList.add("hide");
-  }, 1000);
+  // setTimeout(() => {
+  //   const myProfile = document.querySelector(".my-profile");
+  //   myProfile.classList.add("hide");
+  // }, 1000);
 
-  document.addEventListener("DOMContentLoaded", async function () {
-    const data = await requestApi("load");
-    const mapData = {
-      latLon: [data.latitude, data.longitude],
-      ip: data.ip,
-    };
-    map.createMap(mapData);
-    renderCard(data);
-  });
+  // document.addEventListener("DOMContentLoaded", async function () {
+  //   const data = await requestApi("load");
+  //   const mapData = {
+  //     latLon: [data.latitude, data.longitude],
+  //     ip: data.ip,
+  //   };
+  //   map.createMap(mapData);
+  //   renderCard(data);
+  // });
 
   form.addEventListener("submit", function (event) {
     event.preventDefault();
