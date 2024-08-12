0c7e4dcb047e7f43601144a02deb5e6a9556db59
alamin <codepapa360@gmail.com>
2024-08-12 19:09:38 +0600

    Refactor modal component to use TypeScript and improve error rendering

--- src/components/modal.ts
+++ src/components/modal.ts
@@ -1,15 +1,17 @@
-export function renderError(message) {
-  const modal = document.querySelector(".modal");
-  const msgEl = modal.querySelector(".error-message");
-  const btnModalClose = modal.querySelector(".btn-modal-close");
-  const body = document.querySelector("body");
+export function renderError(message: string): void {
+  const modal = document.querySelector(".modal") as HTMLDivElement;
+  const msgEl = modal.querySelector(".error-message") as HTMLParagraphElement;
+  const btnModalClose = modal.querySelector(
+    ".btn-modal-close"
+  ) as HTMLButtonElement;
+  const body = document.querySelector("body") as HTMLBodyElement;
 
   msgEl.textContent = message;
 
   modal.classList.add("show");
   body.style.overflow = "hidden";
 
-  btnModalClose.addEventListener("click", function () {
+  btnModalClose.addEventListener("click", () => {
     modal.classList.remove("show");
     body.style.overflow = "auto";
   });
